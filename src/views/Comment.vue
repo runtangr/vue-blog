eb99b7304404f459db58fa50f1c4b67aa58a3978
tangr <tangrui@quantumfootball.com>
2018-06-28 22:45:50 +0800

    add disqus comment page

--- src/views/Comment.vue
+++ src/views/Comment.vue
@@ -1,6 +1,8 @@
 <template>
     <div class="comment-box">
-        <div id="disqus_thread"></div>
+
+      <div id="disqus_thread"></div>
+        
     </div>
 </template>
 
@@ -17,21 +19,13 @@ export default {
         },
     },
     mounted() {
-        if (typeof DISQUS !== 'undefined') {
-            DISQUS.reset({
-                reload: true,
-                config: function () {
-                    this.page.identifier = this.sourceId;
-                    this.page.url = location.href;
-                },
-            });
-        }
+        var disqus_shortname = 'runtangr';
+
+        (function() {
+        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+    })();
     },
 };
 </script>
-
-<style lang="stylus" scoped>
-  .comment-box
-    min-height 80px
-    margin-bottom 20px
-</style>
